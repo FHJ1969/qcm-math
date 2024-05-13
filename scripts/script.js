16bcc294a698f1f6ea229c50ad3934075bbdb2ca
Nassim <achnassim34@gmail.com>
2024-05-13 14:47:36 +0200

    Finalisation du HTML de l'index et début du script js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-let btnDemarrer = document.getElementById('btn-demarrer')
+// Je récupère ici les élements HTML lien et boutons 
+
+let lienDemarrer = document.getElementById('lien-demarrer')
+let btnDebutant = document.getElementById('btn-debutant')
+let btnIntermediaire = document.getElementById('btn-intermediaire')
+
+// Je vérifie ici quand le lien est pressé, qu'au moins un des deux choix (btn-debutant/btn-intermediaire) soit pressé
+
+lienDemarrer.addEventListener()
